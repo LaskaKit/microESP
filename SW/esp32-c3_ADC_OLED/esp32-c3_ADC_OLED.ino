8dda5c01e8977a1b14a92c4096ff47db36fba8a2
Konstantin Láska <konstantin.laska@icloud.com>
2023-01-27 09:19:14 +0100

    Update esp32-c3_ADC_OLED.ino

--- SW/esp32-c3_ADC_OLED/esp32-c3_ADC_OLED.ino
+++ SW/esp32-c3_ADC_OLED/esp32-c3_ADC_OLED.ino
@@ -11,8 +11,9 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <ESP32AnalogRead.h> 
+//#include <Adafruit_SSD1306.h>
+#include <Adafruit_SH110X.h>
+#include <ESP32AnalogRead.h> // https://github.com/madhephaestus/ESP32AnalogRead
 
 ESP32AnalogRead adcB;
 #define ADCBpin 0
@@ -23,24 +24,28 @@ ESP32AnalogRead adcS;
 #define sDeviderRatio 3.15           // Voltage devider ratio on ADC pin 1M + 330kOhm
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+//#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define SCREEN_ADDRESS 0x3C // 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 4);
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 4);
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-    
+  Wire.begin(8, 10);  
   Serial.begin(115200);
   delay(100); // let serial console settle
   
   // setting ADC
   adcB.attach(ADCBpin);
   adcS.attach(ADCSpin);
-
+  /*
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
      for(;;); // Don't proceed, loop forever
   }
+  */
+  display.begin(SCREEN_ADDRESS, true);
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
@@ -62,13 +67,14 @@ void loop() {
 
   display.clearDisplay();
 
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setTextSize(1.8);      // Normal 1:1 pixel scale
+  //display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setTextColor(SH110X_WHITE);
   display.setCursor(0, 0);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
-  display.println("Battery" + String(bat_voltage, 3));
-  display.println("Solar" + String(solar_voltage, 3));
+  display.println("Battery: " + String(bat_voltage, 3));
+  display.println("Solar: " + String(solar_voltage, 3));
   
   display.display();
 
