4ab83ba9320594329f38700aaec3baa4c8edf526
petus <petus@users.noreply.github.com>
2024-08-11 21:59:21 +0200

    Update deepsleep_test.ino

--- SW/deepsleep_test/deepsleep_test.ino
+++ SW/deepsleep_test/deepsleep_test.ino
@@ -2,12 +2,18 @@
   *  Example code for deep sleep test with microESP32-C3 v3.x
   *  Code shows how to go to deep sleep mode.
   *  https://www.laskakit.cz/laskakit-microesp/
-  *  laskakit.cz (2023)
+  *  laskakit.cz (2024)
   *
-  *
+  * !!! ESP library version !!!
+  * ESP32 library 3.0.x
+  * Condition: Tools -> USB CDC On Boot must be enabled
+  * use HWCDCSerial instead of USBSerial
+  * ---
+  * ESP32 library 2.0.xy
+  * Condition: Tools -> USB CDC On Boot must be disabled
+  * use USBSerial instead of HWCDCSerial
+  * Board: ESP32-C3 Dev Module
 */
-
-// Must be disabled USB CDC On Boot
 
 #define SLEEP_SEC 15         // Measurement interval (seconds)
 
